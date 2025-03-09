d25da786a4b4830ff3b98eae7f9a92b6a4fa0cc2
farhankhurshidawan <mfarhanprofessional@gmail.com>
2025-03-10 01:50:37 +0500

    all data scrape

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -7,4 +7,16 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        # pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield{
+                'name' : book.css('h3 a::text').get(),
+                'price' : book.css('div.product_price .price_color::text').get(),
+                # 'url' : book.css('h3 a').attrib['href'],
+                'url': response.urljoin(book.css('h3 a').attrib['href']),
+            }
+        next_page=response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+           next_page_url = response.urljoin(next_page)
+        yield response.follow(next_page_url, callback=self.parse)
