78be31c5e65b46aaff3000f4e414f9bca5151c6f
farhankhurshidawan <mfarhanprofessional@gmail.com>
2025-03-10 02:28:58 +0500

    next page diffrent method

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -13,10 +13,13 @@ class BookspiderSpider(scrapy.Spider):
             yield{
                 'name' : book.css('h3 a::text').get(),
                 'price' : book.css('div.product_price .price_color::text').get(),
-                # 'url' : book.css('h3 a').attrib['href'],
-                'url': response.urljoin(book.css('h3 a').attrib['href']),
+                'url' : book.css('h3 a').attrib['href'],
             }
-        next_page=response.css('li.next a ::attr(href)').get()
+        
+        next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
-           next_page_url = response.urljoin(next_page)
-        yield response.follow(next_page_url, callback=self.parse)
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
